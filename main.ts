e5a45768ec3c8352936f279d98cbbd2af3fad54a
moritzdemmerCode <80098065+moritzdemmerCode@users.noreply.github.com>
2021-03-05 10:53:00 +0100

    tolle änderung

--- main.ts
+++ main.ts
@@ -19,4 +19,7 @@ mySprite = sprites.create(img`
     . . . . . . . . . . . . . . . . 
     . . . . . . . . . . . . . . . . 
     `, SpriteKind.Player)
+for (let index = 0; index < 4; index++) {
+    mySprite += 1
+}
 controller.moveSprite(mySprite)
